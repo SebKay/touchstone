efe7c68bf43e6f3fd966c308463a1f7118b31ad7
Seb Kay <seb@sebkay.com>
2021-09-15 20:44:14 +0100

    Add method to create database

--- src/Console/Commands/Setup.php
+++ src/Console/Commands/Setup.php
@@ -15,6 +15,11 @@ class Setup extends Command
     protected $wpZip;
     protected $db_creds = [];
 
+    /**
+     * @var \PDO
+     */
+    protected $db_connection;
+
     public function __construct()
     {
         parent::__construct();
@@ -58,8 +63,9 @@ class Setup extends Command
         $this->addOption(
             'skip-db-creation',
             null,
-            InputOption::VALUE_NONE,
-            "Skip the creation of the database if it already exists"
+            InputOption::VALUE_OPTIONAL,
+            "Skip the creation of the database if it already exists",
+            false
         );
     }
 
@@ -68,26 +74,29 @@ class Setup extends Command
         $output->writeln(\WPTS_CMD_INTRO);
 
         $this->db_creds = [
-            'host' => $input->getOption('db-host') ?: '',
-            'name' => $input->getOption('db-name') ?: '',
-            'user' => $input->getOption('db-user') ?: '',
-            'pass' => $input->getOption('db-pass') ?: '',
+            'host'          => $input->getOption('db-host') ?: '',
+            'name'          => $input->getOption('db-name') ?: '',
+            'user'          => $input->getOption('db-user') ?: '',
+            'pass'          => $input->getOption('db-pass') ?: '',
+            'skip_creation' => $input->getOption('skip-db-creation') ?: false,
         ];
 
         try {
             $this->verifyDatabaseCredentials($output);
-            $this->downloadFiles($input, $output);
-            $this->installFiles($input, $output);
+            $this->connectToDatabase($output);
             $this->createDatabase($input, $output);
-            $this->connectToDatabase($input, $output);
+            // $this->downloadFiles($input, $output);
+            // $this->installFiles($input, $output);
 
             $output->writeln([
+                '',
                 \WPTS_CMD_ICONS['check'] . " Setup complete",
             ]);
 
             return Command::SUCCESS;
         } catch (\Throwable $e) {
             $output->writeln([
+                '',
                 \WPTS_CMD_ICONS['cross'] . " {$e->getMessage()}",
             ]);
 
@@ -99,34 +108,52 @@ class Setup extends Command
     {
         if ($this->db_creds['host'] == '') {
             throw new \InvalidArgumentException("Please provide a database host.");
-
-            return Command::INVALID;
         }
 
         if ($this->db_creds['name'] == '') {
             throw new \InvalidArgumentException("Please provide a database name.");
-
-            return Command::INVALID;
         }
 
         if ($this->db_creds['user'] == '') {
             throw new \InvalidArgumentException("Please provide a database user.");
-
-            return Command::INVALID;
         }
 
         if ($this->db_creds['pass'] == '') {
             throw new \InvalidArgumentException("Please provide a database password.");
-
-            return Command::INVALID;
         }
+    }
 
+    public function connectToDatabase($output)
+    {
         $output->writeln(\WPTS_CMD_ICONS['loading'] . ' Testing database connection...');
 
-        if (1 == 2) {
-            throw new \Exception('There was an error connecting to the database. Please check the credentials.');
+        try {
+            $db_connection = new \PDO(
+                "mysql:host={$this->db_creds['host']};charset=UTF8",
+                $this->db_creds['user'],
+                $this->db_creds['pass']
+            );
 
-            return Command::INVALID;
+            $db_connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+            $this->db_connection = $db_connection;
+        } catch (\PDOException $e) {
+            throw new \Exception('There was an error connecting to the database. Please check the credentials.');
+        }
+    }
+
+    public function createDatabase(InputInterface $input, OutputInterface &$output)
+    {
+        if ($input->getOption('skip-db-creation')) {
+            return;
+        }
+
+        $output->writeln(\WPTS_CMD_ICONS['loading'] . ' Creating database...');
+
+        try {
+            $this->db_connection->query("CREATE DATABASE IF NOT EXISTS {$this->db_creds['name']}");
+        } catch (\Exception $e) {
+            throw new \Exception("There was an error creating the database.");
         }
     }
 
@@ -138,8 +165,6 @@ class Setup extends Command
 
         if ($version_check->getStatusCode() != 200 || !\property_exists($version_check_response, 'offers')) {
             throw new \Exception('There was an error getting the latest version of WordPress. Please check your connection.');
-
-            return Command::INVALID;
         }
 
         return $version_check_response->offers[0]->download;
@@ -155,8 +180,6 @@ class Setup extends Command
 
         if ($wp_files->getStatusCode() != 200) {
             throw new \Exception('There was an error downloading WordPress. Please check your connection.');
-
-            return Command::INVALID;
         }
 
         \file_put_contents($this->wpZip, $wp_files->getBody()->getContents());
@@ -168,33 +191,17 @@ class Setup extends Command
 
         if (!\file_exists($this->wpZip)) {
             throw new \InvalidArgumentException('No WordPress test files found.');
-
-            return Command::INVALID;
         }
 
         $zip = new \ZipArchive();
 
         if ($zip->open($this->wpZip) != true) {
             throw new \Exception('There was an error unzipping the WordPress files.');
-
-            return Command::INVALID;
         }
 
         $zip->extractTo(sys_get_temp_dir());
         $zip->close();
 
         $output->writeln(\WPTS_CMD_ICONS['loading'] . ' Installing test files...');
-    }
-
-    public function createDatabase(InputInterface $input, OutputInterface &$output)
-    {
-        if (!$input->getOption('skip-db-creation')) {
-            $output->writeln(\WPTS_CMD_ICONS['loading'] . ' Creating database...');
-        }
-    }
-
-    public function connectToDatabase(InputInterface $input, OutputInterface &$output)
-    {
-        $output->writeln(\WPTS_CMD_ICONS['loading'] . ' Connecting to database...');
     }
 }
