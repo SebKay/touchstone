decec4b84e40075aca961cbc49bd9af7aa3d21bf
Seb Kay <seb@sebkay.com>
2021-09-25 20:51:38 +0100

    Run tests from project phpunit.xml and not package phpunit.xml in 'prod' mode

--- src/Console/Commands/Test.php
+++ src/Console/Commands/Test.php
@@ -52,6 +52,7 @@ class Test extends Command
 
         if ($this->env == 'prod') {
             $this->phpunitExecutablePath = __DIR__ . '/../../../../../../vendor/bin/phpunit';
+            $this->phpunitConfigPath     = __DIR__ . '/../../../../../../phpunit.xml';
         }
 
         try {
